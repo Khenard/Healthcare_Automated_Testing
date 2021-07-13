12a2bb21bf349fb2765c34ee0b693175941d4f2b
khena <khena@DESKTOP-9M35TAL>
2021-07-13 13:51:22 +0800

    fix fix

--- healthcare/healthcarePackage/draft.py
+++ healthcare/healthcarePackage/draft.py
@@ -20,20 +20,73 @@ datafile = os.getcwd()+"\data.xlsx"
 excel_data = pd.read_excel(datafile)
 #age = pd.DataFrame(excel_data, columns =['NAME'])
 #convert column data to array list and use it to select random values
-colname = excel_data['NAME'].tolist()
-print(colname)
-name_random = random.choice(colname)
+col_name = excel_data['NAME'].tolist()
+print(col_name)
+name_random = random.choice(col_name)
+    
+wound_df = pd.read_excel(datafile, 'wound_assessment')
+
+# Randomize location
+col_location = wound_df['LOCATION'].tolist()
+location = random.choice(col_location)
+
+# Randomize stages
+col_stages = wound_df['STAGES'].tolist()
+stages = int(random.choice(col_stages))
+
+# Randomize grantissue
+col_grantissue = wound_df['GRANTISSUE'].tolist()
+grantissue = int(random.choice(col_grantissue))
+
+# Randomize nectissue
+col_nectissue = wound_df['NECTISSUE'].tolist()
+nectissue = int(random.choice(col_nectissue))
+
+# Randomize granneccoverage
+col_granneccoverage = wound_df['GRANNECCOVERAGE'].tolist()
+granneccoverage = int(random.choice(col_granneccoverage))
+
+# Randomize granneccoverage
+col_exuamount = wound_df['EXUAMOUNT'].tolist()
+exuamount = int(random.choice(col_exuamount))
+
+
+
+
+
+print(location)
+print(stages)
+print(grantissue)
+print(nectissue) 
+print(granneccoverage)
+print(exuamount) 
+print(exutype)
+print(edges) 
+print(periwoundtissue)
+print(healingstatus) 
+print(woundrelatedpain) 
+
     
 servers.qaserver()
-config.driver.get("https://qado.medisource.com/patients/admitted")
-time.sleep(5) 
-stb = config.driver.find_element_by_xpath('//*[@id="searchbar__wrapper"]/div/input').send_keys(name_random)
+config.driver.get("https://qado.medisource.com/patientcare/CEB7010B-2FE7-45B1-808C-3C25C0E594F0/857A6F1E-C5A3-415E-92FC-879D983FAA2F/woundcare/update/435CC653-F0D9-4E4A-96C2-C16763973186/1")
 
 
 
+    
 
-
-
+function_woundprocess.completewoundassessment(
+    stages, 
+    grantissue, 
+    nectissue, 
+    granneccoverage, 
+    exuamount, 
+    exutype, 
+    edges, 
+    periwoundtissue,
+    healingstatus, 
+    woundrelatedpain
+    )
+    
 
 
 
